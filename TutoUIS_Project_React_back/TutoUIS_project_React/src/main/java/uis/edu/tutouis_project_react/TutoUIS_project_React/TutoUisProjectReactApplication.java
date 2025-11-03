2115fe34ee119c84574e52bb4ec23badd045cc85
William Urrutia <162224301+UrrutiaTs17@users.noreply.github.com>
2025-11-02 21:58:19 -0500

    Inicializacion backend proyecto con springboot

---
+++ TutoUIS_Project_React_back/TutoUIS_project_React/src/main/java/uis/edu/tutouis_project_react/TutoUIS_project_React/TutoUisProjectReactApplication.java
@@ -0,0 +1,13 @@
+package uis.edu.tutouis_project_react.TutoUIS_project_React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoUisProjectReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoUisProjectReactApplication.class, args);
+	}
+
+}
---
+++ TutoUIS_Project_React_back/TutoUIS_project_React/src/test/java/uis/edu/tutouis_project_react/TutoUIS_project_React/TutoUisProjectReactApplicationTests.java
@@ -0,0 +1,13 @@
+package uis.edu.tutouis_project_react.TutoUIS_project_React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoUisProjectReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
